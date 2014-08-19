a0d768316d0066b1abc1318c8dbb0deb05ae3e3c
Aydin <aydin@wearejh.com>
2014-08-19 17:08:26 +0100

    Fix product link

--- app/code/community/Atwix/PVF/Block/Adminhtml/Widget/Button.php
+++ app/code/community/Atwix/PVF/Block/Adminhtml/Widget/Button.php
@@ -3,22 +3,17 @@
 class Atwix_PVF_Block_Adminhtml_Widget_Button extends Mage_Adminhtml_Block_Widget_Button
 {
     /**
-     * @var Mage_Catalog_Model_Product Product instance
-     */
-    private $_product;
-
-    /**
      * Block construct, setting data for button, getting current product
      */
     protected function _construct()
     {
-        $this->_product = Mage::registry('current_product');
+        $product = Mage::registry('current_product');
         parent::_construct();
         $this->setData(array(
             'label'     => Mage::helper('catalog')->__('View Product Page'),
-            'onclick'   => 'window.open(\''.Mage::getModel('core/url')->getUrl() . $this->_product->getUrlPath() .'\')',
-            'disabled'  => !$this->_isVisible(),
-            'title' => (!$this->_isVisible())?
+            'onclick'   => sprintf("window.open('%s')", $product->getProductUrl()),
+            'disabled'  => !$this->isVisible($product),
+            'title' => (!$this->isVisible($product))?
                 Mage::helper('catalog')->__('Product is not visible on frontend'):
                 Mage::helper('catalog')->__('View Product Page')
         ));
@@ -27,10 +22,11 @@ class Atwix_PVF_Block_Adminhtml_Widget_Button extends Mage_Adminhtml_Block_Widge
     /**
      * Checking product visibility
      *
+     * @param Mage_Catalog_Model_Product $product
      * @return bool
      */
-    private function _isVisible()
+    private function isVisible(Mage_Catalog_Model_Product $product)
     {
-        return $this->_product->isVisibleInCatalog() && $this->_product->isVisibleInSiteVisibility();
+        return $product->isVisibleInCatalog() && $product->isVisibleInSiteVisibility();
     }
 }
