8b486212dc6ea104d4f1dcbbcb482347e4d76078
Aydin Hassan <aydin@hotmail.co.uk>
2014-09-03 12:54:27 +0100

    Fix for simple product create via config product

--- app/code/community/Atwix/PVF/Model/Observer.php
+++ app/code/community/Atwix/PVF/Model/Observer.php
@@ -4,14 +4,15 @@ class Atwix_PVF_Model_Observer
 {
     public function addPVFButton($observer)
     {
-        $_block = $observer->getBlock();
-        $_type = $_block->getType();
-        if ($_type == 'adminhtml/catalog_product_edit') {
-            $_deleteButton = $_block->getChild('delete_button');
-            $_block->setChild('product_view_button',
-                $_block->getLayout()->createBlock('atwix_pvf/adminhtml_widget_button')
-            );
-            $_deleteButton->setBeforeHtml($_block->getChild('product_view_button')->toHtml());
+        $block  = $observer->getBlock();
+        $type   = $block->getType();
+        if ($type === 'adminhtml/catalog_product_edit') {
+            if ($deleteButton = $block->getChild('delete_button')) {
+                $block->setChild('product_view_button',
+                    $block->getLayout()->createBlock('atwix_pvf/adminhtml_widget_button')
+                );
+                $deleteButton->setBeforeHtml($block->getChild('product_view_button')->toHtml());
+            }
         }
     }
 }
